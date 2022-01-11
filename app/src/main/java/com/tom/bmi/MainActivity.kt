0336faf18557a2133567128a75e0a049b212661e
Jazoe <jazoe59@yahoo.com.tw>
2022-01-12 03:42:37 +0800

    CH11

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -16,21 +16,56 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//       object : View.OnClickListener() {
+//           @override
+//           fun OnClick(v: View) {
+//           }
+//       }
+        binding.bHelp.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("BMI 說明")
+                .setMessage(getString(R.string.help_info))
+                .setPositiveButton("OK", null)
+//                .setNeutralButton("Cancel", null)
+                .show()
+        }
+        binding.bBMI.setOnClickListener {
+            val weight = binding.edWeight.text.toString().toFloat()
+            val height = binding.edHeight.text.toString().toFloat()
+            var bmi : Float = 0f
+
+            if (height != 0f) {
+                bmi = weight / (height*height)
+            } else {
+                bmi = 0f
+            }
+
+            Log.d("BMI", bmi.toString())
+            //Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+
+            AlertDialog.Builder(this)
+                .setMessage(bmi.toString())
+                .setTitle(getString(R.string.your_bmi))
+                .setPositiveButton("OK", null)
+                .setNeutralButton("Cancel", null)
+                .show()
+        }
     }
 
-    fun bmi(view: View) {
-        val weight = binding.edWeight.text.toString().toFloat()
-        val height = binding.edHeight.text.toString().toFloat()
-        val bmi = weight / (height*height)
-
-        Log.d("BMI", bmi.toString())
-        //Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
-
-        AlertDialog.Builder(this)
-            .setMessage(bmi.toString())
-            .setTitle(getString(R.string.your_bmi))
-            .setPositiveButton("OK", null)
-            .setNeutralButton("Cancel", null)
-            .show()
-    }
+//    fun bmi(view: View) {
+//        val weight = binding.edWeight.text.toString().toFloat()
+//        val height = binding.edHeight.text.toString().toFloat()
+//        val bmi = weight / (height*height)
+//
+//        Log.d("BMI", bmi.toString())
+//        //Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+//
+//        AlertDialog.Builder(this)
+//            .setMessage(bmi.toString())
+//            .setTitle(getString(R.string.your_bmi))
+//            .setPositiveButton("OK", null)
+//            .setNeutralButton("Cancel", null)
+//            .show()
+//   }
 }
