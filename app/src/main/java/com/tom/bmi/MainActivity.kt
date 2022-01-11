3343afbc3636fa8f5dc22a8ba826de2499ef5c9c
Jazoe <jazoe59@yahoo.com.tw>
2022-01-12 02:59:28 +0800

    CH10

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tom.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +24,13 @@ class MainActivity : AppCompatActivity() {
         val bmi = weight / (height*height)
 
         Log.d("BMI", bmi.toString())
+        //Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle(getString(R.string.your_bmi))
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
     }
 }
